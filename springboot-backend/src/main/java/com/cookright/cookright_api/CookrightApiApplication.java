413e8742ace853b0c896d85ace6397c2465237af
Ryan Dinh <rqd3qmk@virginia.edu>
2025-08-11 13:11:51 -0400

    initalized springboot backend project

---
+++ springboot-backend/src/main/java/com/cookright/cookright_api/CookrightApiApplication.java
@@ -0,0 +1,13 @@
+package com.cookright.cookright_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookrightApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookrightApiApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/src/test/java/com/cookright/cookright_api/CookrightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookright.cookright_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookrightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
